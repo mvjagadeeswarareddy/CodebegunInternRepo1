3dda04702ac74a3feef1f5f790da58680ec243ba
mvjagadeeswarareddy <janijagadeesreddy@gmail.com>
2024-11-16 19:34:05 +0530

    jagadeesBkp
    BackupTest

---
+++ InternJavaProject/src/com/test/demo/jagadeesbkp.java
@@ -0,0 +1,16 @@
+package com.test.demo;
+
+public class CodeBegun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println("Hello");
+
+		System.out.println("Hello CODEBEGUN");
+		System.out.println("Easy to Learn");
+
+	}
+
+}
